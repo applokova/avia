a08d599ad2174776b90e6e6d8cef5efc2fcef52a
applokova <stasya133@mail.ru>
2022-08-15 21:02:47 +0300

    доработка2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ package src;
 public class Main {
     public static void main(String[] args) {
 
-        int a = 5350;
-        int b = 20;
+        int price = 5350;
+        int oneMile = 20;
 
-        int c = a / b;
-        System.out.println(c);
+        int bonus = price / oneMile;
+        System.out.println(bonus);
     }
 }
